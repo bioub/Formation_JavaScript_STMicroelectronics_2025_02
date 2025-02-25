6c1aba1a3e331c8049b939f589b6dfe852f841e5
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2025-02-25 12:12:51 +0100

    Exemple CommonJs

---
+++ modules/main.js
@@ -0,0 +1,7 @@
+const MyMath = require('./my-math');
+const hello = require('./hello');
+
+console.log(MyMath.sum(1, 2)); // 3
+console.log(MyMath.multiply(2, 3)); // 6
+
+console.log(hello('Romain')); // Hello, Romain!
---
+++ modules/my-math.js
@@ -0,0 +1,14 @@
+// function(exports, require, module) {
+
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+exports.sum = sum;
+exports.multiply = multiply;
+
+// }
