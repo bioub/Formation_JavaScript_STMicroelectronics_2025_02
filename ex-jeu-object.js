90481bfac3dbeb8823515c33407ea738f15336d4
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2025-02-25 11:12:32 +0100

    Correction Objet

--- ex-jeu-object.js
+++ ex-jeu-object.js
@@ -2,72 +2,74 @@
 // qui contient ces 4 fonctions de sorte
 // à pouvoir écrire Random.getRandomInt(0, 100) par exemple
 
-function getRandom() {
-  return Math.random();
-}
-
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function getRandomInt(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-}
-
-function getRandomIntInclusive(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-}
+const Random = {
+  getRandom() {
+    return Math.random();
+  },
+  getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  },
+  getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  },
+  getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+  },
+};
 
 // require importe l'API readline de Node.js
 const readline = require("readline");
-
-// Configurer readline
-// pour lire sur le clavier (process.stdin)
-// et écrire dans le terminal (process.stdout)
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-function loop() {
-  if (essais.length) {
-    console.log("Vous avez déjà saisi : " + essais.join(', '));
+class Jeu {
+  constructor(options) {
+    const min = options?.min ?? 0;
+    const max = options?.max ?? 100; // équivalent à (options && options.max) ?? 100;
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    this.essais = [];
+    this.entierAlea = Random.getRandomIntInclusive(min, max);
   }
-
-  // La question s'affiche tout suite (synchrone)
-  // on attend la réponse de l'utilisateur en asynchrone
-  rl.question("Quel est le nombre ? ", (answer) => {
-    console.log(`Vous avez saisi : ${answer}`);
-
-    const entierSaisi = Number.parseInt(answer, 10);
-
-    if (Number.isNaN(entierSaisi)) {
-      console.log("Erreur : Il faut saisir un nombre");
-      return loop();
+  loop() {
+    if (this.essais.length) {
+      console.log("Vous avez déjà saisi : " + this.essais.join(', '));
     }
 
-    essais.push(entierSaisi);
+    // La question s'affiche tout suite (synchrone)
+    // on attend la réponse de l'utilisateur en asynchrone
+    this.rl.question("Quel est le nombre ? ", (answer) => {
+      console.log(`Vous avez saisi : ${answer}`);
 
-    if (entierSaisi < entierAlea) {
-      console.log("Trop petit");
-      loop();
-    } else if (entierSaisi > entierAlea) {
-      console.log("Trop grand");
-      loop();
-    } else {
-      console.log("Gagné");
-      rl.close();
-    }
-  });
+      const entierSaisi = Number.parseInt(answer, 10);
+
+      if (Number.isNaN(entierSaisi)) {
+        console.log("Erreur : Il faut saisir un nombre");
+        return this.loop();
+      }
+
+      this.essais.push(entierSaisi);
+
+      if (entierSaisi < this.entierAlea) {
+        console.log("Trop petit");
+        this.loop();
+      } else if (entierSaisi > this.entierAlea) {
+        console.log("Trop grand");
+        this.loop();
+      } else {
+        console.log("Gagné");
+        this.rl.close();
+      }
+    });
+  }
 }
 
-const essais = [];
-const entierAlea = getRandomIntInclusive(0, 100);
-loop();
+
+const game = new Jeu();
+game.loop();
 
 // 2 - Créer une classe Jeu
 // dont loop deviendra une méthode
